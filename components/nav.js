369208da00cc07b5ee4117a1000d692daf84c55f
Natanlimap <natanlimap@gmail.com>
2020-12-09 13:27:13 -0300

    rolagem

--- components/nav.js
+++ components/nav.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link } from "react-scroll";
 
 
 
@@ -13,10 +14,10 @@ export default function MyNavbar({ fixed }) {
           <button  onClick={closeNav} className='text-white pr-5 pt-5'>x</button>
         </div>
         <div className='flex text-left flex-col ml-5'>
-          <a href='/' style={{ fontSize: '25px'}} className='text-white text-3xl mx-5'><p>Origens</p></a>
-          <a href='/' style={{ fontSize: '25px'}} className='text-white text-3xl mx-5'><p>Produtos</p></a>
-          <a href='/' style={{ fontSize: '25px'}} className='text-white text-3xl mx-5'><p>Loja</p></a>
-          <a href='/' style={{ fontSize: '25px'}} className='text-white text-3xl mx-5'><p>Contato</p></a>
+          <Link to={'Origens'} smooth={true} style={{ fontSize: '25px'}} className='text-white text-3xl mx-5'><p>Origens</p></Link>
+          <Link to={'Produtos'} smooth={true} style={{ fontSize: '25px'}} className='text-white text-3xl mx-5'><p>Produtos</p></Link>
+          <Link to={'Loja'} smooth={true} style={{ fontSize: '25px'}} className='text-white text-3xl mx-5'><p>Loja</p></Link>
+          <Link to={'Contato'} smooth={true} style={{ fontSize: '25px'}} className='text-white text-3xl mx-5'><p>Contato</p></Link>
         </div>
       </div>
     )
@@ -30,17 +31,17 @@ export default function MyNavbar({ fixed }) {
   return (
     <div>
       {teste}
-      <div className='py-5 lg:mx-40 mx-10 flex flex-row justify-between'>
+      <div className='py-10  mx-10 flex flex-row justify-center'>
         <div className='lg:w-1/4 '>
           <img className='lg:w-2/4 w-2/4' style={{minWidth: '10px'}} src='./nav-logo.svg'></img>
         </div>
         <div className='hidden md:flex flex-row my-auto'>
-          <a href='/' className='text-white mx-5'><p>Origens</p></a>
-          <a href='/' className='text-white mx-5'><p>Produtos</p></a>
-          <a href='/' className='text-white mx-5'><p>Loja</p></a>
-          <a href='/' className='text-white mx-5'><p>Contato</p></a>
+           <Link href='/' to={'Origens'} smooth={true} className='text-white text-xl mx-5'><p>Origens</p></Link>
+           <Link href='/' to={'Produtos'} smooth={true} className='text-white text-xl mx-5'><p>Produtos</p></Link>
+           <Link href='/' to={'Loja'} smooth={true} className='text-white text-xl mx-5'><p>Loja</p></Link>
+           <Link href='/' to={'Contato'} smooth={true} className='text-white text-xl mx-5'><p>Contato</p></Link>
         </div>
-        <a href='/' className='flex md:hidden' onClick={openNav}><img src='./hamburguer.svg'></img></a>
+        <a className='flex md:hidden' onClick={openNav}><img src='./hamburguer.svg'></img></a>
       </div>
     </div>
   );
