3cc3d7b48666d94829f96d86499e232206817215
Natanlimap <natanlimap@gmail.com>
2020-12-09 13:03:26 -0300

    wpp adicionado

--- pages/index.js
+++ pages/index.js
@@ -9,14 +9,17 @@ import Sal from '../components/Sal'
 export default function IndexPage() {
   return (
     <div className='w-screen body'>
-      <Header/>
+      <a href="https://api.whatsapp.com/send?phone=+558434220100" class="float" target="_blank">
+        <i class="my-float"><img src='./wpp.png'></img></i>
+      </a>
+      <Header />
       <Origens />
       <Sal />
       <Produtos />
       <Loja />
       <Contato />
       <footer id='Footer' className='flex flex-row justify-center'>
-        <img style={{width: '5rem'}} className='py-5 my-auto'  src='./agencia.svg'></img>
+        <img style={{ width: '5rem' }} className='py-5 my-auto' src='./agencia.svg'></img>
       </footer>
     </div>
   )
