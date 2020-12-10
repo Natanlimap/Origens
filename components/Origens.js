ba9dc925f6beda9710d4c7b583061699bf5d1d85
Natanlimap <natanlimap@gmail.com>
2020-12-10 16:30:56 -0300

    Finalizado 2.3

--- components/Origens.js
+++ components/Origens.js
@@ -6,14 +6,18 @@ export default function Origens() {
         <div id='Origens-fundo' >
             <div className='md:pt-10 pt-40' id='Origens'>
                 <div className='lg:grid lg:grid-cols-7 lg:grid-cols-7   flex flex-row justify-center text-center' >
-                    <div className='xl:col-start-4 xl:col-end-80 md:mr-36 lg:col-start-4 lg:col-end-8 xl:ml-20 lg:ml-32  md:col-end-5 lg:pt-0 md:pt-32 md:mt-0  mt-20 mt-10'>
+                    <div className='xl:col-start-4 xl:col-end-8 mr-36 lg:col-start-4 lg:col-end-8 xl:ml-20 lg:ml-32  md:col-end-5 lg:pt-0 md:pt-32 md:mt-0  mt-20 mt-10'>
                         <Image
                             src='/origens-logo.svg'
                             width={300}
                             height={100}
                         ></Image>
                         <div className='lg:mt-5 lg:text-lg mx-10 md:text-right text-center text-white text-base '>
-                            <p className=''>Origens traz em seu nome sua razão de existir que é procurar nas fontes mais nobres um sal com sabor e qualidade que, diferentemente do sal comum, não precisa passar pelo processo de refinamento que tira os minerais tão importantes do sal.</p>
+                            <p>Origens traz em seu nome sua razão de existir que é</p>
+                            <p>procurar nas fontes mais nobres um sal com sabor e</p>
+                            <p>qualidade que, diferentemente do sal comum, não precisa</p>
+                            <p>passar pelo processo de refinamento que tira os minerais</p>
+                            <p>tão importantes do sal.</p>
                         </div>
                     </div>
                 </div>
