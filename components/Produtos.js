5110b382b1724f3269a16a092435fff06b31371c
Natanlimap <natanlimap@gmail.com>
2020-12-10 10:31:19 -0300

    Finalizado 2.1

--- components/Produtos.js
+++ components/Produtos.js
@@ -13,21 +13,21 @@ export default function Produtos() {
                 <div className='mx-auto mt-5 text-center'>
                     <Image width={400} height={400} className='mt-2 mx-auto lg:px-30 md:px-20 sm:px-30 px-32' src='/produto-azul.svg'></Image>
                     <p className='mt-5 mx-20 font-light'>Sal marinho moído de <strong className='font-bold'>Mossoró</strong> | Sea Salt</p>
-                    <Image width={100} height={100} className='px-24 py-5 ' src='/produto-mais.svg' />
+                    <Image width={200} height={100} className='px-24 py-5 ' src='/produto-mais.svg' />
                     <p className='font-bold'>1KG</p>
                     <p className='text-sm'>Net Wt. 35,2 oz</p>
                 </div>
                 <div className='mx-auto mt-5 text-center'>
                     <Image width={400} height={400} className='mt-2 mx-auto lg:px-30 md:px-20 sm:px-30 px-32' src='/produto-verde.svg'></Image>
                     <p className='mt-5 mx-20 font-light'>Sal marinho moído de <strong className='font-bold'>Mossoró</strong> | Sea Salt</p>
-                    <Image width={100} height={100} className='px-24 py-5' src='/produto-mais.svg' />
+                    <Image width={200} height={100} className='px-24 py-5' src='/produto-mais.svg' />
                     <p className='font-bold'>1KG</p>
                     <p className='text-sm'>Net Wt. 35,2 oz</p>
                 </div>
                 <div className='mx-auto mt-5 text-center'>
                     <Image width={400} height={400} className='mt-2 mx-auto lg:px-30 md:px-20 sm:px-30 px-32' src='/produto-rosa.svg'></Image>
                     <p className='mt-5 mx-20 font-light'>Sal marinho moído de <strong className='font-bold'>Mossoró</strong> | Sea Salt</p>
-                    <Image width={100} height={100} className='px-24 py-5' src='/produto-puro.svg' />
+                    <Image width={200} height={100} className='px-24 py-5' src='/produto-puro.svg' />
                     <p className='font-bold'>500g</p>
                     <p className='text-sm'>Net Wt. 17,6 oz</p>
                 </div>
