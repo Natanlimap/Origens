447c3eaa639e20f674f5a4bb8d2870217cd4584f
Natanlimap <natanlimap@gmail.com>
2020-12-10 16:16:54 -0300

    Finalizado 2.3

--- components/Origens.js
+++ components/Origens.js
@@ -6,7 +6,7 @@ export default function Origens() {
         <div id='Origens-fundo' >
             <div className='md:pt-10 pt-40' id='Origens'>
                 <div className='lg:grid lg:grid-cols-7 lg:grid-cols-7   flex flex-row justify-center text-center' >
-                    <div className='xl:col-start-4 lg:col-start-4 lg:col-end-8 xl:ml-20 lg:ml-32  md:col-end-5 lg:pt-0 md:pt-32 md:mt-0  mt-20 mt-10'>
+                    <div className='xl:col-start-4 xl:col-end-7 lg:col-start-4 lg:col-end-8 xl:ml-20 lg:ml-32  md:col-end-5 lg:pt-0 md:pt-32 md:mt-0  mt-20 mt-10'>
                         <Image
                             src='/origens-logo.svg'
                             width={300}
